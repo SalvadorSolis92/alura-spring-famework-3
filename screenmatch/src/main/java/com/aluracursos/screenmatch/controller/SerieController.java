8f3bc850e44aee1cd07377023d56d3cf046e0e48
Salvador Solis <salvador920110@gmail.com>
2024-05-29 19:12:22 -0600

    endpoints para consumir

--- screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -6,19 +6,31 @@ import com.aluracursos.screenmatch.repository.SerieRepository;
 import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService service;
 
-    @GetMapping("/series")
-    public List<SerieDTO> mostrarMensaje(){
+    @GetMapping("/")
+    public List<SerieDTO> obtenerSeries(){
         return service.obtenerTodasLasSeries();
+    }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obtenerTop5(){
+        return service.obtenerTop5();
+    }
+
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> obtenerLanzamientosRecientes(){
+        return service.obtenerLanzamientosRecientes();
     }
 }
--- screenmatch/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmatch.service;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.model.Episodio;
+import com.aluracursos.screenmatch.model.Serie;
 import com.aluracursos.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,21 @@ public class SerieService {
     private SerieRepository repository;
 
     public List<SerieDTO> obtenerTodasLasSeries(){
-        return repository.findAll().stream()
+        return convertirDatos(repository.findAll());
+    }
+
+    public List<SerieDTO> obtenerTop5() {
+        return convertirDatos(repository.findTop5ByOrderByEvaluacionDesc());
+    }
+
+    public List<SerieDTO> obtenerLanzamientosRecientes(){
+        return convertirDatos(repository.lanzamientosRecientes());
+    }
+
+    public List<SerieDTO> convertirDatos(List<Serie> series){
+        return series.stream()
                 .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getGenero(),
                         s.getPoster(), s.getActores(), s.getSinopsis()))
                 .collect(Collectors.toList());
     }
-
 }
